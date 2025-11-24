d9cca48fe3d8b053ce7ae9b519397577940362c4
Aidan <aidannowakowski@gmail.com>
2025-11-24 18:13:51 -0500

    func testing notebook and weekly stats

--- src/data_collection.py
+++ src/data_collection.py
@@ -11,12 +11,12 @@ def get_player_stats(league, player_name):
     # print(player_stats._get_value(0, 'Pass TD'))
     return player_stats
 
-def get_team_from_num(oauth, team_num):
+def get_team_from_num(oauth, curr_week, team_num):
     if team_num > 12 or team_num < 1:
         return "Invalid team number"
     team_code = str(curr_season_code) + ".l.6288.t." + str(team_num)
     team = Team(oauth, team_key=team_code)
-    roster = team.roster(1)
+    roster = team.roster(curr_week)
     return pd.json_normalize(roster)
 
 def get_team_yearly_stats(league, team):
@@ -34,3 +34,22 @@ def get_team_yearly_stats(league, team):
 def get_position_stats(team_stats, pos):
     pos_mask = team_stats.eligible_positions.apply(lambda x: str(pos) in x)
     return team_stats[pos_mask]
+
+def get_weekly_totals(league, team):
+    total_weeks = 17
+    player_id_list = team['player_id'].tolist()
+    weekly_data = pd.DataFrame(team['name'])
+    #week_1_data = league.player_stats(player_id_list, req_type='week', week=1)
+    #weekly_data = pd.concat([week_1_data['name'], week_1_data['total_points']], axis=1, keys=['name', '1'])
+    for i in range(1, 17):
+        new_week = league.player_stats(player_id_list, req_type='week', week=i)
+        new_week = pd.json_normalize(new_week)
+        new_week['total_points'] = pd.to_numeric(new_week['total_points'])
+        new_totals = new_week['total_points'].tolist()
+        weekly_data.insert(i, str(i), new_totals, True)
+    return weekly_data
+        
+
+
+
+
